2e98196895ee3b4997b4fdada690cf3d00e19b23
Xyyaua <2690833249@qq.com>
2024-05-01 20:01:03 +0800

    🌈 style(main.gd): remove unused code

--- src/main.gd
+++ src/main.gd
@@ -3,24 +3,22 @@ extends Node2D
 @onready var world_json_data = Util.get_json_data(FileAccess.open("res://Resource/data/world.json", FileAccess.READ))
 @onready var map_json_data = Util.get_json_data(FileAccess.open("res://Resource/data/map.json", FileAccess.READ))
 @onready var Ui = %Ui
-var Settings := preload("res://src/system/setting/setting.tscn")
 
 func _process(delta) -> void:
+	#Debug
 	Global.mouse_pos = get_global_mouse_position()
 	Global.camera_pos = Camera.position
 	
 	Global.camera_zoom = Camera.zoom
-	
+
 func _ready() -> void:
 	%Setting.set_visible(false)
 	
 	init()
-	
 
 func init() -> void:
 	var err = load_data()
 	if err == OK:
-	#load_data()
 		init_ui()
 		Ui.set_ui_type(0)
 		MapManager.set_map_to("10101")
@@ -30,15 +28,8 @@ func load_data() -> Error:
 		Global.world_data[i["id"]] = i
 		
 		for map in map_json_data["map"][i["id"]]:
-			#if !map.has("is_separator"):
 			Global.map_data[map["id"]] = map
 			Global.map_data[map["id"]]["world"] = i["id"]
-		
-		
-		#for map in map_json_data["map"]:
-			#Global.map_data[map] = {}
-			#for md in map_json_data["map"][map]:
-				#Global.map_data[map][md["id"]] = md
 	
 	Global.floorTemp = map_json_data["floorTemplate"]
 	print("完成Global Data加载")
