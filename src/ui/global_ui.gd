75766143312deff58b670ca099dc949a67435b40
Xyyaua <2690833249@qq.com>
2024-05-19 00:38:14 +0800

    🌈 style: Change repository URL

--- src/ui/global_ui.gd
+++ src/ui/global_ui.gd
@@ -3,9 +3,9 @@ extends CanvasLayer
 func _ready() -> void:
 	var ver = ProjectSettings.get_setting("application/config/version", null)
 	if ver != null:
-		%BtnVersion.set_text("V. " + ver)
+		%BtnVersion.set_text("Ver. " + ver)
 	else:
 		%BtnVersion.set_text("Test Build")
 
 func _on_btn_version_pressed() -> void:
-	OS.shell_open("https://github.com/Xyyaua/StarRail-Map")
+	OS.shell_open("https://github.com/XYY-Studio/StarRail-Map")
